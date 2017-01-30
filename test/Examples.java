e26a0350241a43b6817ac116d04cc1603a103a70
Kyle Corry <kylecorry31@gmail.com>
2017-01-29 21:34:42 -0500

    Add Vector3 test cases

--- test/Examples.java
+++ test/Examples.java
@@ -12,7 +12,7 @@ import org.junit.Test;
  */
 public class Examples {
     @Test
-    public void testPoint(){
+    public void testPoint() {
         Point point = new Point(1, 2, 3);
         Point otherPoint = new Point(3, 2, 1);
 
@@ -28,7 +28,34 @@ public class Examples {
     }
 
     @Test
-    public void testPose(){
+    public void testVector3() {
+        Vector3 vector3 = new Vector3(1, 2, 3);
+        Vector3 otherVector3 = new Vector3(3, 4, 0);
+
+        assertEquals(new Vector3(0, 0, 0), vector3.subtract(vector3));
+        assertEquals(new Vector3(0, 1, 2), vector3.subtract(1));
+        assertEquals(new Vector3(2, 4, 6), vector3.add(vector3));
+        assertEquals(new Vector3(2, 3, 4), vector3.add(1));
+        assertEquals(new Vector3(2, 4, 6), vector3.multiply(2));
+        assertEquals(Vector3.zero, vector3.multiply(0));
+        assertNotEquals(vector3, otherVector3);
+        assertEquals(new Vector3(0.5, 1, 1.5), vector3.divide(2));
+        assertEquals(new Vector3(1, 2, 3), new Vector3(new Point(1, 2, 3)));
+        assertEquals(new Vector3(1, 0, 0), Vector3.i);
+        assertEquals(new Vector3(0, 1, 0), Vector3.j);
+        assertEquals(new Vector3(0, 0, 1), Vector3.k);
+        assertEquals(11, vector3.dot(otherVector3), 0);
+        assertEquals(0, Vector3.i.dot(Vector3.k), 0);
+        assertEquals(Vector3.i, Vector3.j.cross(Vector3.k));
+        assertEquals(Vector3.j, Vector3.i.cross(Vector3.k));
+        assertEquals(Vector3.k, Vector3.i.cross(Vector3.j));
+        assertEquals(Vector3.k, new Vector3(0, 0, 8).normalize());
+        assertEquals(new Vector3(3 / 5.0, 4 / 5.0, 0), otherVector3.normalize());
+
+    }
+
+    @Test
+    public void testPose() {
         Pose pose = new Pose(new Point(1, 2, 3), new Quaternion(90, Vector3.k));
 
         assertEquals(new Point(1, 2, 3), pose.position);
@@ -36,7 +63,7 @@ public class Examples {
     }
 
     @Test
-    public void testQuaternion(){
+    public void testQuaternion() {
         Quaternion quaternion = new Quaternion(90, Vector3.k);
         Quaternion quaternion1 = new Quaternion(0, Vector3.k);
 
