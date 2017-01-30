666ba708e790c68464aa9028abf5b27b11add81d
Kyle Corry <kylecorry31@gmail.com>
2017-01-30 16:23:13 -0500

    Add JavaDocs for Pose

--- src/com/kylecorry/tf/Pose.java
+++ src/com/kylecorry/tf/Pose.java
@@ -1,11 +1,20 @@
 package com.kylecorry.tf;
 
+/**
+ * A point and orientation in 3D.
+ */
 public class Pose {
-	public Point position;
-	public Quaternion orientation;
+    public Point position;
+    public Quaternion orientation;
 
-	public Pose(Point position, Quaternion orientation) {
-		this.position = position;
-		this.orientation = orientation;
-	}
+    /**
+     * Create a pose representation a position with an orientation.
+     *
+     * @param position    The position.
+     * @param orientation The orientation as a quaternion.
+     */
+    public Pose(Point position, Quaternion orientation) {
+        this.position = position;
+        this.orientation = orientation;
+    }
 }
