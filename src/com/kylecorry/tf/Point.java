4eabb7c38db79d15efbda860ca5c0d7d917a75b1
Kyle Corry <kylecorry31@gmail.com>
2017-01-29 21:45:16 -0500

    Improved documentation of Point
    Added JavaDocs and changed parameter names to better document the Point
    class.

--- src/com/kylecorry/tf/Point.java
+++ src/com/kylecorry/tf/Point.java
@@ -2,66 +2,125 @@ package com.kylecorry.tf;
 
 /**
  * The position of a point in 3D
- *
  */
 public class Point {
-	public double x, y, z;
+    public double x, y, z;
 
-	public static final Point origin = new Point(0, 0, 0);
+    public static final Point origin = new Point(0, 0, 0);
 
-	public Point(double x, double y, double z) {
-		this.x = x;
-		this.y = y;
-		this.z = z;
-	}
+    /**
+     * Create a point in 3D space.
+     *
+     * @param x The x position of the point.
+     * @param y The y position of the point.
+     * @param z The z position of the point.
+     */
+    public Point(double x, double y, double z) {
+        this.x = x;
+        this.y = y;
+        this.z = z;
+    }
 
-	public Point add(Point other) {
-		return new Point(x + other.x, y + other.y, z + other.z);
-	}
+    /**
+     * Add two points together.
+     *
+     * @param transform The point to add.
+     * @return A point which is the addition of this point and the other point.
+     */
+    public Point add(Point transform) {
+        return new Point(x + transform.x, y + transform.y, z + transform.z);
+    }
 
-	public Point add(double other) {
-		return new Point(x + other, y + other, z + other);
-	}
+    /**
+     * Move a point by a scalar.
+     *
+     * @param transform The scalar to add to the point.
+     * @return The point moved along each of its axes by the scalar.
+     */
+    public Point add(double transform) {
+        return new Point(x + transform, y + transform, z + transform);
+    }
 
-	public Point add(Vector3 other) {
-		return new Point(x + other.x, y + other.y, z + other.z);
-	}
+    /**
+     * Move a point by a vector.
+     *
+     * @param transform The vector to move to the point by.
+     * @return The point moved by the vector.
+     */
+    public Point add(Vector3 transform) {
+        return new Point(x + transform.x, y + transform.y, z + transform.z);
+    }
 
-	public Point subtract(Point other) {
-		return new Point(x - other.x, y - other.y, z - other.z);
-	}
+    /**
+     * Subtract two points.
+     *
+     * @param transform The point to subtract.
+     * @return A point which is the difference of this point and the other point.
+     */
+    public Point subtract(Point transform) {
+        return new Point(x - transform.x, y - transform.y, z - transform.z);
+    }
 
-	public Point subtract(double other) {
-		return new Point(x - other, y - other, z - other);
-	}
+    /**
+     * Move a point in the opposite direction by a scalar.
+     *
+     * @param transform The scalar to subtract from the point.
+     * @return The point moved along each of its axes by the scalar in the opposite direction.
+     */
+    public Point subtract(double transform) {
+        return new Point(x - transform, y - transform, z - transform);
+    }
 
-	public Point subtract(Vector3 other) {
-		return new Point(x - other.x, y - other.y, z - other.z);
-	}
+    /**
+     * Move a point by a vector in the opposite direction.
+     *
+     * @param transform The vector to move to the point by.
+     * @return The point moved by the vector in the opposite direction.
+     */
+    public Point subtract(Vector3 transform) {
+        return new Point(x - transform.x, y - transform.y, z - transform.z);
+    }
 
-	public Point multiply(double other) {
-		return new Point(x * other, y * other, z * other);
-	}
+    /**
+     * Scale the point by a scalar.
+     *
+     * @param scaleFactor The scale factor.
+     * @return The point scaled by the scale factor.
+     */
+    public Point multiply(double scaleFactor) {
+        return new Point(x * scaleFactor, y * scaleFactor, z * scaleFactor);
+    }
 
-	public Point divide(double other) {
-		return new Point(x / other, y / other, z / other);
-	}
-	
-	public Vector3 toVector(){
-		return new Vector3(this);
-	}
-	
-	@Override
-	public boolean equals(Object obj) {
-		if (obj == null || !(obj instanceof Point))
-			return false;
-		Point other = (Point) obj;
-		return other.x == x && other.y == y && other.z == z;
-	}
+    /**
+     * Scale the point by a scalar (1/scalar).
+     *
+     * @param scaleFactor The scale factor which will be inverted.
+     * @return The point scaled by the inverted scale factor.
+     */
+    public Point divide(double scaleFactor) {
+        return new Point(x / scaleFactor, y / scaleFactor, z / scaleFactor);
+    }
 
-	@Override
-	public String toString() {
-		return "<Point " + x + ", " + y + ", " + z + ">";
-	}
+    /**
+     * Convert the point to a vector from the origin.
+     *
+     * @return The vector representation of the point.
+     */
+    public Vector3 toVector() {
+        return new Vector3(this);
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == null || !(obj instanceof Point))
+            return false;
+        Point other = (Point) obj;
+        return other.x == x && other.y == y && other.z == z;
+    }
+
+    @Override
+    public String toString() {
+        return "<Point " + x + ", " + y + ", " + z + ">";
+    }
 
 }
