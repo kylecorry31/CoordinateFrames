1bb4aa5fdd85078d0664a5a3db661440dbc13cda
Kyle Corry <kylecorry31@gmail.com>
2017-01-29 21:25:17 -0500

    Add more test cases
    Added test cases for Pose and started adding test cases for Quaternion.
    More test cases are needed for Quaternion operations.

--- test/Examples.java
+++ test/Examples.java
@@ -2,6 +2,8 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotEquals;
 
 import com.kylecorry.tf.Point;
+import com.kylecorry.tf.Pose;
+import com.kylecorry.tf.Quaternion;
 import com.kylecorry.tf.Vector3;
 import org.junit.Test;
 
@@ -23,6 +25,27 @@ public class Examples {
         assertNotEquals(point, otherPoint);
         assertEquals(new Point(0.5, 1, 1.5), point.divide(2));
         assertEquals(new Vector3(1, 2, 3), point.toVector());
+    }
+
+    @Test
+    public void testPose(){
+        Pose pose = new Pose(new Point(1, 2, 3), new Quaternion(90, Vector3.k));
+
+        assertEquals(new Point(1, 2, 3), pose.position);
+        assertEquals(new Quaternion(90, Vector3.k), pose.orientation);
+    }
+
+    @Test
+    public void testQuaternion(){
+        Quaternion quaternion = new Quaternion(90, Vector3.k);
+        Quaternion quaternion1 = new Quaternion(0, Vector3.k);
+
+        assertEquals(new Quaternion(0, Vector3.i), quaternion1);
+        assertEquals(new Quaternion(0, Vector3.j), quaternion1);
+        assertEquals(new Quaternion(-90, Vector3.k), quaternion.inverse());
+        assertEquals(new Quaternion(0, Vector3.k), quaternion1.inverse());
+        assertEquals(new Quaternion(90, Vector3.k), quaternion.multiply(quaternion1));
+        // TODO: Add more test cases for quaternion operations
     }
 
 }
