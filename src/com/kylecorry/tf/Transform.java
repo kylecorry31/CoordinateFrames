d3b4dfee012b2e3a3bb3326d5d98ed0a0b63867e
Kyle Corry <kylecorry31@gmail.com>
2017-01-30 16:37:03 -0500

    Add JavaDocs for the Transform

--- src/com/kylecorry/tf/Transform.java
+++ src/com/kylecorry/tf/Transform.java
@@ -1,23 +1,37 @@
 package com.kylecorry.tf;
 
+/**
+ * A representation of a transform in 3D space.
+ */
 public class Transform {
-	public Vector3 translation;
-	public Quaternion rotation;
+    public Vector3 translation;
+    public Quaternion rotation;
 
-	public Transform(Vector3 translation, Quaternion rotation) {
-		this.translation = translation;
-		this.rotation = rotation;
-	}
+    /**
+     * Create a transform with a translation and rotation.
+     *
+     * @param translation The translation portion of the transform.
+     * @param rotation    The rotation portion of the transform.
+     */
+    public Transform(Vector3 translation, Quaternion rotation) {
+        this.translation = translation;
+        this.rotation = rotation;
+    }
 
-	public Transform inverse() {
-		return new Transform(translation.multiply(-1), rotation.inverse());
-	}
+    /**
+     * Computes the inverse of the transform.
+     *
+     * @return The inverse transform.
+     */
+    public Transform inverse() {
+        return new Transform(translation.multiply(-1), rotation.inverse());
+    }
 
-	@Override
-	public boolean equals(Object obj) {
+    @Override
+    public boolean equals(Object obj) {
         if (obj == null || !(obj instanceof Transform))
             return false;
         Transform other = (Transform) obj;
         return other.rotation.equals(rotation) && other.translation.equals(translation);
-	}
+    }
 }
