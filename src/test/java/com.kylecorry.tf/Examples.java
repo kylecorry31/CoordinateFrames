97dcea32750dae4f6bddc1f73b77ba962b18eaa0
Kyle Corry <kylecorry31@gmail.com>
2017-02-11 16:47:27 -0500

    Removed unused imports

--- src/test/java/com.kylecorry.tf/Examples.java
+++ src/test/java/com.kylecorry.tf/Examples.java
@@ -1,7 +1,6 @@
 package com.kylecorry.tf;
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotEquals;
 import static org.junit.Assert.assertTrue;
 
 import com.kylecorry.geometry.Point;
